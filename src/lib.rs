99b3fbd3a091d0417d626fddd1459d25a7b6fe19
Richard Walters <rwalters@digitalstirling.com>
2021-01-21 18:06:20 -0800

    Add tests for `list_processes`

--- src/lib.rs
+++ src/lib.rs
@@ -32,3 +32,37 @@ pub use macos::list_processes;
 
 #[cfg(target_os = "windows")]
 pub use windows::list_processes;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        convert::TryFrom as _,
+        env::current_exe,
+        net::{
+            Ipv4Addr,
+            TcpListener,
+        },
+    };
+
+    #[test]
+    fn find_self_by_image_path() {
+        let processes = list_processes();
+        let self_path = current_exe().unwrap().canonicalize().unwrap();
+        let self_id = usize::try_from(std::process::id()).unwrap();
+        assert!(processes.iter().any(|process| {
+            process.image == self_path && process.id == self_id
+        }));
+    }
+
+    #[test]
+    fn find_self_by_tcp_server_port() {
+        let tcp = TcpListener::bind((Ipv4Addr::UNSPECIFIED, 0)).unwrap();
+        let port = tcp.local_addr().unwrap().port();
+        let processes = list_processes();
+        let self_id = usize::try_from(std::process::id()).unwrap();
+        assert!(processes.iter().any(|process| {
+            process.tcp_server_ports.contains(&port) && process.id == self_id
+        }));
+    }
+}
